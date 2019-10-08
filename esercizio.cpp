5d0e5c8d7eae05d698ae7dde1a3e10851a4cf890
Marco-01 <marcocecoro01@outlook.it>
2019-10-08 10:46:58 +0200

    Stampa della somma

--- esercizio.cpp
+++ esercizio.cpp
@@ -6,4 +6,13 @@ int main() {
 	
 	int n1,n2;
 	
+	cout<<"Inserisci il primo numero da sommare"<<endl;
+	cin>>n1;
+	
+	cout<<"Inserisci il secondo numero da sommare"<<endl;
+	cin>>n2;
+	
+	cout<<"Il risultato e' "<<n1+n2<<endl;
+	
+	
 }
